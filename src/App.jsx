bc66b3255cfa339795cf1bb33644fda9dab31768
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-21 16:38:26 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.scss"
+
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallex</section>
+    <section>Services</section>
+    <section>Parallex</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Kontakt</section>
+  </div>;
+};
+
+export default App;
