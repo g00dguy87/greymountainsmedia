1bbaaf3d77e04312eff5d32e5ca36cd7cf4fbfa6
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-23 15:20:31 +0100

    Add files via upload

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,9 +4,20 @@ import "./hero.scss"
 const Hero = () => {
   return (
     <div className='hero'>
-      <div className='imageContainer'>
-        <img src='hero.png' alt='gm' />
-      </div>
+        <div className='wrapper'>
+            <div className='textContainer'>
+                <h2>Grey Mountains Media</h2>
+                <h1>Deine rundum Agentur für alles</h1>
+                <div className='buttons'>
+                    <button>Mehr Erfahren</button>
+                    <button>Kontaktieren</button>
+                </div>
+                <img src='/scroll.png' alt='scroll' />
+            </div>
+        </div>  
+        <div className='imageContainer'>
+            <img src='hero.png' alt='gm' />
+        </div>
     </div>
   )
 }
