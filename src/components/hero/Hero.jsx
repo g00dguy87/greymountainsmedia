518b9667a013003bc1c112374d1a89d32a93f667
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-28 19:49:07 +0100

    Add files via upload

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import "./hero.scss"
 
+const textVariants = {
+    initial:{
+        x:-500,
+        opacity: 0,
+    },
+    animate: {
+        x:0,
+        opacity:1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        }
+    },
+}
+
 const Hero = () => {
   return (
     <div className='hero'>
         <div className='wrapper'>
-            <div className='textContainer'>
-                <h2>Grey Mountains Media</h2>
-                <h1>Deine rundum Agentur für alles</h1>
-                <div className='buttons'>
-                    <button>Mehr Erfahren</button>
-                    <button>Kontaktieren</button>
+            <div className='textContainer' variants={textVariants}>
+                <h2 variants={textVariants}>Grey Mountains Media</h2>
+                <h1 variants={textVariants}>Deine rundum Agentur für alles</h1>
+                <div variants={textVariants} className='buttons'>
+                    <button variants={textVariants}>Mehr Erfahren</button>
+                    <button variants={textVariants}>Kontaktieren</button>
                 </div>
-                <img src='/scroll.png' alt='scroll' />
+                <img variants={textVariants} src='/scroll.png' alt='scroll' />
             </div>
         </div>
         <div className='slidingTextContainer'>
